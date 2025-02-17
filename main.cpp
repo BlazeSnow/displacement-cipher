95d220d28f12fbf3b0d7595e796df7c348f10571
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2025-02-17 22:05:55 +0800

    v1.0.2 (#3)
    * update
    
    * Update main.cpp
    
    * Update main.cpp
    
    * Update main.cpp
    
    * update

--- main.cpp
+++ main.cpp
@@ -1,72 +1,54 @@
-﻿// Copyright (C) 2024 BlazeSnow
-// 保留所有权利
-// 本程序以GNU General Public License v3.0的条款发布
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
+
+// 定义密码偏移量
 #define PASSWORD 5
 
-void bianma(char* p, int number)
-{
-	for (int i = 0; i <= number - 1; i++, p++)
-	{
-		*p = (char)((int)*p + PASSWORD);
-		cout << *p;
-	}
-	cout << endl;
+// 定义数组
+vector<char> p;
+
+// 编码密码
+void bianma() {
+  for (auto i : p) {
+    cout << (char)((int)i + PASSWORD);
+  }
 }
 
-void jiema(char* p, int number)
-{
-	for (int i = 0; i <= number - 1; i++, p++)
-	{
-		*p = (char)((int)*p - PASSWORD * 2);
-		cout << *p;
-	}
-	cout << endl;
+// 解码密码
+void jiema() {
+  for (auto i : p) {
+    cout << (char)((int)i - PASSWORD);
+  }
 }
 
-int main()
-{
-	system("chcp 54936");
-	cout << "Copyright (C) 2024 BlazeSnow.保留所有权利。" << endl;
-	cout << "本程序以GNU General Public License v3.0的条款发布。" << endl;
-	cout << "当前程序版本号：v1.0.1" << endl;
-	cout << "https://github.com/BlazeSnow/displacement-cipher" << endl
-		<< endl;
-	int number = 1000;
-	char* p = (char*)malloc(sizeof(char) * number);
-	// 检查p的NULL
-	if (!p)
-	{
-		cout << "ERROR:malloc" << endl;
-		system("pause");
-		return -1;
-	}
-	// 输入
-	cout << "移位密码移位距离：" << PASSWORD << endl;
-	cout << "请输入内容：" << endl;
-	for (int i = 0; i <= number; i++)
-	{
-		*(p + i) = (char)cin.get();
-		if (*(p + i) == '\n')
-		{
-			number = i;
-			break;
-		}
-	}
-	// 输出PASSWORD的值
-	cout << endl;
-	cout << "移位密码移位距离：" << PASSWORD << endl;
-	// 编码
-	cout << "编码的结果为:" << endl;
-	bianma(p, number);
-	// 解码
-	cout << "解码的结果为:" << endl;
-	jiema(p, number);
-	// 释放p
-	free(p);
-	system("pause");
-	return 0;
+int main() {
+  system("chcp 65001");
+  system("cls");
+  cout << "Copyright (C) 2024-2025 BlazeSnow. 保留所有权利。" << endl;
+  cout << "当前程序版本号：v1.0.2" << endl;
+  cout << "https://github.com/BlazeSnow/displacement-cipher" << endl << endl;
+  // 输入
+  cout << "移位密码移位距离：" << PASSWORD << endl;
+  cout << "请输入内容：" << endl;
+  string input;
+  getline(cin, input);
+  p = vector<char>(input.begin(), input.end());
+  // 输出PASSWORD的值
+  cout << endl;
+  cout << "移位密码移位距离：" << PASSWORD << endl << endl;
+  // 编码
+  cout << "编码的结果为:" << endl;
+  bianma();
+  cout << endl << endl;
+  // 解码
+  cout << "解码的结果为:" << endl;
+  jiema();
+  cout << endl << endl;
+  // 结束前暂停
+  system("pause");
+  return 0;
 }
