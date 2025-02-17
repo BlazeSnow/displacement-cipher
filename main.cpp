47c7071878067ab9deeda73ec439a4ba5f7723c3
BlazeSnowSSH <129476965+BlazeSnow@users.noreply.github.com>
2025-02-17 21:57:26 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 ﻿#include <cstdlib>
 #include <iostream>
+#include <string>
 #include <vector>
 
 using namespace std;
@@ -12,17 +13,15 @@ vector<char> p;
 
 // 编码密码
 void bianma() {
-  for (int i = 0; i <= p.size(); i++) {
-    p[i] = (char)((int)p[i] + PASSWORD);
-    cout << p[i];
+  for (int i = 0; i < p.size(); i++) {
+    cout << (char)((int)p[i] + PASSWORD);
   }
 }
 
 // 解码密码
 void jiema() {
-  for (int i = 0; i <= p.size(); i++) {
-    p[i] = (char)((int)p[i] - PASSWORD * 2);
-    cout << p[i];
+  for (int i = 0; i < p.size(); i++) {
+    cout << (char)((int)p[i] - PASSWORD);
   }
 }
 
@@ -35,24 +34,20 @@ int main() {
   // 输入
   cout << "移位密码移位距离：" << PASSWORD << endl;
   cout << "请输入内容：" << endl;
-  while (true) {
-    char temp = (char)cin.get();
-    if (temp == '\n') {
-      break;
-    }
-    p.push_back(temp);
-  }
+  string input;
+  getline(cin, input);
+  p = vector<char>(input.begin(), input.end());
   // 输出PASSWORD的值
   cout << endl;
-  cout << "移位密码移位距离：" << PASSWORD << endl;
+  cout << "移位密码移位距离：" << PASSWORD << endl << endl;
   // 编码
   cout << "编码的结果为:" << endl;
   bianma();
-  cout << endl;
+  cout << endl << endl;
   // 解码
   cout << "解码的结果为:" << endl;
   jiema();
-  cout << endl;
+  cout << endl << endl;
   // 结束前暂停
   system("pause");
   return 0;
