f356cfc2dca948586c95d69ac11c9089519f7e58
BlazeSnow <git@blazesnow.org>
2026-09-03 16:50:21 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,34 +3,57 @@
 #include <cstring>
 #include <vector>
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 using namespace std;
 
 // 定义密码偏移量
 #define PASSWORD 5
+
+void clearConsole() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
+void enterToContinue() {
+    printf("按回车键继续...");
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {
+    }
+    getchar();
+}
 
 // 定义数组
 vector<char> p;
 
 // 编码密码
 void bianma() {
-    for (auto i : p) {
-        printf("%c", (char)((int)i + PASSWORD));
+    for (auto i: p) {
+        printf("%c", (char) ((int) i + PASSWORD));
     }
 }
 
 // 解码密码
 void jiema() {
-    for (auto i : p) {
-        printf("%c", (char)((int)i - PASSWORD));
+    for (auto i: p) {
+        printf("%c", (char) ((int) i - PASSWORD));
     }
 }
 
 int main() {
-    system("chcp 65001");
-    system("cls");
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
+
+    clearConsole();
     printf("Copyright (C) 2024-2026 BlazeSnow. 保留所有权利。\n");
-    printf("当前程序版本号：v1.0.3\n");
-    printf("https://github.com/BlazeSnow/CppWorkspace\n\n");
+    printf("https://github.com/BlazeSnow/displacement-cipher\n\n");
     // 输入
     printf("移位密码移位距离：%d\n", PASSWORD);
     printf("请输入内容：\n");
@@ -49,6 +72,6 @@ int main() {
     jiema();
     printf("\n\n");
     // 结束前暂停
-    system("pause");
+    enterToContinue();
     return 0;
 }
